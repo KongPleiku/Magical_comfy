1d5a58f9d27ea590a817d714033ee8afa145dca9
KongPleiku <154966174+KongPleiku@users.noreply.github.com>
2026-02-27 23:39:23 +0700

    temp add gallery

---
+++ src/services/gallery_services.py
@@ -0,0 +1,10 @@
+from utils.singleton import SingletonMeta
+from utils.event_bus import event_bus
+
+
+class Gallery_service(metaclass=SingletonMeta):
+    def __init__(self):
+        pass
+
+    def save_temporary_image(self):
+        pass
--- src/views/gallery_view.py
+++ src/views/gallery_view.py
@@ -1,4 +1,6 @@
 import flet as ft
+import os
+import base64
 from loguru import logger
 
 
@@ -9,17 +11,73 @@ class Gallery_View(ft.View):
             padding=0,
             spacing=0,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            auto_scroll=True,
         )
         self.page = page
+
+        self.temporary_tab = ft.Tab(
+            text="Recent",
+            content=ft.Column(
+                controls=[
+                    ft.Row(
+                        controls=[
+                            ft.Container(
+                                content=ft.Image(
+                                    src="https://picsum.photos/1080/1920",
+                                    width=200,
+                                    height=200,
+                                    fit=ft.ImageFit.CONTAIN,
+                                ),
+                                margin=10,
+                            ),
+                        ]
+                    )
+                ],
+                scroll=ft.ScrollMode.AUTO,
+            ),
+        )
+
+        self.permanent_tab = ft.Tab(
+            text="Gallery",
+            content=ft.Column(
+                controls=[],
+                scroll=ft.ScrollMode.AUTO,
+            ),
+        )
+
+        self.navigation_tabs = ft.Tabs(
+            selected_index=0,
+            animation_duration=300,
+            expand=True,
+            scrollable=True,
+            tabs=[self.temporary_tab, self.permanent_tab],
+        )
+
         self.controls = [
             ft.AppBar(
-                title=ft.Text("Gallery"),
-                bgcolor=ft.colors.SURFACE_VARIANT,
+                title=ft.Row(
+                    controls=[
+                        ft.Icon(ft.Icons.IMAGE),
+                        ft.Text("Gallery"),
+                    ],
+                    spacing=10,
+                    vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                ),
+                actions=[
+                    ft.IconButton(
+                        icon=ft.Icons.REFRESH,
+                    ),
+                    ft.IconButton(
+                        icon=ft.Icons.CLOSE,
+                        on_click=self._go_to_main,
+                    ),
+                ],
+                bgcolor=ft.Colors.GREY_800,
             ),
-            ft.Text("Settings content goes here"),
-            ft.ElevatedButton("Go to Main", on_click=self._go_to_main),
+            self.navigation_tabs,
         ]
-        logger.info("Setting_View initialized.")
+        logger.info("Gallery_View initialized.")
 
     def _go_to_main(self, e):
+        logger.info("Going to main")
         self.page.go("/main")
